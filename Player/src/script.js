e680433aab4726c4ecacf5f3293d45222d0f6cf1
Cammyxalot <renoir.fab@outlook.com>
2022-08-20 11:31:04 +0200

    fix

--- Player/src/script.js
+++ Player/src/script.js
@@ -19,8 +19,8 @@ songIndex = 1
 loadSong(songs[songIndex])
 function loadSong(song) {
   title.innerText = song
-  audio.src = `/Player/Music/${song}.mp3`
-  cover.src = `/Cover/${song}.jpeg`
+  audio.src = `./Player/Music/${song}.mp3`
+  cover.src = `./Cover/${song}.jpeg`
 }
 // function to play and stop the music
 
